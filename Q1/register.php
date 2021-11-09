82f36a7babe0cd0e7248d5fe8bb8ceebdc276f7a
Senthil Sivaraman <vaivassaravanan1@gmail.com>
2021-11-09 20:33:41 +0530

    q1 done

--- Q1/register.php
+++ Q1/register.php
@@ -1,37 +1,81 @@
+
+
 <?php
+$db = mysqli_connect('localhost', 'root', '', 'FS-Lab');  
+if(isset($_POST['name']))
+{  
     $name = $_POST['name'];
     $fname = $_POST['fname'];
     $sid = $_POST['sid'];
     $email = $_POST['email'];
-    $dept = $_POST['dept']; 
-    $db = mysqli_connect('localhost', 'root', '', 'registration');  
-    echo $name;
+    $slot = $_POST['slot']; 
+
+    $q = "SELECT * FROM Schedule WHERE id='{$slot}'";
+    $result = mysqli_query($db,$q);
+    if(mysqli_num_rows($result)==1)
+    {
+        $row = mysqli_fetch_assoc($result);
+        $available = (int)$row['available'];
+        $available = $available - 1;
+
+        echo $available;
+    }
+    $q = "UPDATE Schedule SET available = '{$available}' WHERE id='{$slot}'";
+    $result = mysqli_query($db,$q);
+    if($result)
+    {
+        echo "Success";
+    }
+
+    $query = "INSERT INTO labs VALUES('{$name}','{$fname}','{$sid}','{$email}','{$slot}')";
+    $result = mysqli_query($db,$query);
+    if($result)
+    {
+        echo '<script>alert("Successfully Registered")</script>';
+    }
+    else
+    {
+        echo '<script>alert("Error")</script>';
+    }   
+    }   
+
 ?>
+
 <html>
     <head>
         <title>Registration</title>
     </head>
     <body>
         <h1>Registration</h1>
-        <form method="post">
+        <form action="register.php" method="post">
             <label for="name">Name</label>
-            <input type="text" id="name">
+            <input type="text" name="name" id="name">
 
             <label for="name">First Name</label>
-            <input type="text" id="fname">
+            <input type="text" name="fname">
 
             <label for="name">SID</label>
-            <input type="text" id="sid">
+            <input type="text" name="sid">
 
             <label for="name">Email</label>
-            <input type="email" id="email">
-
-            <select name="" id="">
-                <option value="CSE">CSE</option>
+            <input type="email" name="email">
+            
+            <select name="slot" id="slot">
+        <?php
+            $query = "SELECT * FROM Schedule";
+            $res = mysqli_query($db, $query);
+            if(mysqli_num_rows($res) > 0)
+            {
+                while($row = mysqli_fetch_assoc($res))
+                {
+                    echo "<option value='".$row['id']."'>".$row['slot']." ".$row['available']." Remaining</option>";   
+                }
+            }
+        ?>
             </select>
+                
+            
             <button type="submit">Submit</button>
         </form>
     </body>
 </html>
-
-
